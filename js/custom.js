40bd16e391c17f69f4291a2a41bd520f4a7eca66
kbarath3694 <kbarath3694@gmail.com>
2021-11-13 10:14:43 +0530

    New_Commit_11-13-21

--- js/custom.js
+++ js/custom.js
@@ -17,7 +17,7 @@ var mybutton = document.getElementById("btnScrollToTop");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -29,3 +29,5 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
